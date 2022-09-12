8b8948bbf3a427f2f7412a530808b3e0a0ee9019
Manish Anand <manishanandprogrammer@gmail.com>
2022-09-12 20:50:52 +0530

    Starting Spring Validations

---
+++ src/main/java/com/manish/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.manish.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
