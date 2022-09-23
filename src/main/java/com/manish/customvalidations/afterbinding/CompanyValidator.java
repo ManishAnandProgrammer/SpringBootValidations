aa1b35f0bbf25ae847792dee69703bb170db164e
Manish Anand <manishanandprogrammer@gmail.com>
2022-09-23 21:21:51 +0530

    Customer Care Number Validation Added But Still Showing Default Messages, Need To Check How To Resolve Messages IN Case Of Custom-Validator

--- src/main/java/com/manish/customvalidations/afterbinding/CompanyValidator.java
+++ src/main/java/com/manish/customvalidations/afterbinding/CompanyValidator.java
@@ -1,6 +1,7 @@
 package com.manish.customvalidations.afterbinding;
 
 import com.manish.dtos.requests.CompanyInput;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -17,11 +18,16 @@ public class CompanyValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         CompanyInput companyInput = (CompanyInput) target;
-
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address",
             "company.address.required", "Address Is Mandatory");
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name",
                 "company.name.required", "Name Is Mandatory");
+
+        if (StringUtils.isBlank(companyInput.getCustomerCareNumber())) {
+            errors.rejectValue("customerCareNumber", "company.customerCareNumber.required",
+                    new Object[]{"hello", "world", "to"},
+                    "Hey There, Customer Care Number Is Mandatory");
+        }
     }
 }
--- src/main/java/com/manish/dtos/requests/CompanyInput.java
+++ src/main/java/com/manish/dtos/requests/CompanyInput.java
@@ -8,4 +8,5 @@ import lombok.Setter;
 public class CompanyInput {
     private String name;
     private String address;
+    private String customerCareNumber;
 }
