458de4840c9155b79409d3fa8cfb2efa27421052
Manish Anand <manishanandprogrammer@gmail.com>
2025-02-04 11:36:46 +0530

    DriverPayload Added Comments.

--- src/main/java/com/manish/constants/ValidationConstant.java
+++ src/main/java/com/manish/constants/ValidationConstant.java
@@ -1,5 +1,9 @@
 package com.manish.constants;
 
 public final class ValidationConstant {
+    private ValidationConstant() {
+        throw new UnsupportedOperationException("No Object For This Class.!");
+    }
+
     public static final int NAME_MAX_LENGTH = 100;
 }
--- src/main/java/com/manish/dtos/requests/DriverPayload.java
+++ src/main/java/com/manish/dtos/requests/DriverPayload.java
@@ -14,14 +14,17 @@ public class DriverPayload {
     @NotBlank
     private String name;
 
+    // profile picture can be one only
     @ValidFileIdsPayload(min = 1, max = 1, optional = false, groups = OnCreate.class)
     @ValidFileIdsPayload(min = 1, max = 1, optional = false, groups = OnUpdate.class)
     private FileIdsPayload profilePicture;
 
+    // license can be two only, one pic of front and one pic from beck
     @ValidFileIdsPayload(min = 2, max = 2, optional = true, groups = OnCreate.class)
     @ValidFileIdsPayload(min = 2, max = 2, optional = true, groups = OnUpdate.class)
     private FileIdsPayload license;
 
+    // personal documents can be 3 to 5 only
     @ValidFileIdsPayload(min = 3, max = 5, optional = false, groups = OnCreate.class)
     @ValidFileIdsPayload(min = 3, max = 5, optional = false, groups = OnUpdate.class)
     private FileIdsPayload personalDocuments;
